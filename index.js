c98e3bcfa71a27d7d482166cf71b1b51e5b04e8e
oscarfernandass <125243046+oscarfernandass@users.noreply.github.com>
2023-11-28 10:27:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ const port = process.env.PORT || 8080;
 server.use(middlewares);
 server.use(router);
 
+// Use the watch function to enable real-time updates
+const watchOptions = {
+  watch: true,
+  noCors: false, // Enable CORS, adjust as needed
+};
+
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
+  jsonServer.watch("db.json", watchOptions); // Enable watching for changes
 });
