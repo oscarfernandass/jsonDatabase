a3dd119ec64dd2dc0e225076307761b42ef48199
oscarfernandass <125243046+oscarfernandass@users.noreply.github.com>
2023-11-28 11:14:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const jsonServer = require("json-server");
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
+const chokidar = require("chokidar");
 
 const adapter = new FileSync("db.json");
 const db = low(adapter);
@@ -13,13 +14,16 @@ const port = process.env.PORT || 8080;
 server.use(middlewares);
 server.use(router);
 
-// Use the watch function to enable real-time updates
-const watchOptions = {
-  watch: true,
-  noCors: false, // Enable CORS, adjust as needed
-};
+// Use chokidar to watch for changes in db.json
+const watcher = chokidar.watch("db.json");
+
+watcher.on("change", () => {
+  console.log("Reloading database...");
+  // Reload the database when changes occur
+  db.read();
+  server.emit("hot-reload");
+});
 
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
-  jsonServer.watch("db.json", watchOptions); // Enable watching for changes
 });
