6ac2d90a352339ca705173541beb7567959b8d30
oscarfernandass <125243046+oscarfernandass@users.noreply.github.com>
2023-11-29 19:02:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-const jsonServer = require("json-server");
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
-const chokidar = require("chokidar");
-
-const adapter = new FileSync("db.json");
-const db = low(adapter);
-
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(db);
+const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3200; // <== You can change the port
 
 server.use(middlewares);
 server.use(router);
 
-// Use chokidar to watch for changes in db.json
-const watcher = chokidar.watch("db.json");
-
-watcher.on("change", () => {
-  console.log("Reloading database...");
-  // Reload the database when changes occur
-  db.read();
-  server.emit("hot-reload");
-});
-
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+server.listen(port);
